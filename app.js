580b58001af0b8187617c3b260dc7f075c7a7849
Gabo2447 <gabrieldeolival2@gmail.com>
2025-05-04 18:03:58 -0400

    feat(new): :sparkles: 'Sobre mi' ha sido agregado
    Se ha mejorado los proyectos y la seccion sobre mi ha sido implementada

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hi")
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.header__nav');
+const navLinks = document.querySelectorAll('.nav__list-a');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+  navToggle.classList.toggle('open');
+});
+
+// Cierra el menú al hacer click en un enlace
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+    navToggle.classList.remove('open');
+  });
+});
